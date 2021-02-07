6545a81992b2f5cd7c2cc8a8adb14a5b85dc2496
Alex Jackson <alex@alexj.org>
2021-02-07 11:03:25 +0000

    Add patches to force RGB output over DisplayPort
    Not 100% sure these are necessary as to my eye the DisplayPort output looked
    correct to begin with. These patches will remove any potential ambiguity though.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ OVERRIDE_DIR=/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-1e6d
 FILE_EXT=.plist
 
 HDMI_PATCH_FILE=DisplayProductID-5b80.plist
+DISPLAYPORT_PATCH_FILE=DisplayProductID-5b7f.plist
 
 if ! [ -d $OVERRIDE_DIR ]
 then
@@ -12,5 +13,8 @@ then
          mkdir -p $OVERRIDE_DIR
 fi
 
-sudo -p "Root password to copy patch files: " \
+sudo -p "Root password to copy HDMI patch: " \
      cp -i "$REPOSITORY_ROOT/$HDMI_PATCH_FILE" "$OVERRIDE_DIR/$(basename "$HDMI_PATCH_FILE" "$FILE_EXT")"
+
+sudo -p "Root password to copy DisplayPort patch: " \
+     cp -i "$REPOSITORY_ROOT/$DISPLAYPORT_PATCH_FILE" "$OVERRIDE_DIR/$(basename "$DISPLAYPORT_PATCH_FILE" "$FILE_EXT")"
